02f5d12b657d6f6c7bdbd4b9583de187c1d3fb0d
Quang Nguyen <worldforce2011@gmail.com>
2012-02-07 00:09:01 +0700

    fix validating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,8 @@ class Review < ActiveRecord::Base
   belongs_to  :user, class_name: 'Spree::User'
 
   validates_presence_of :review
-  validates_presence_of :user_id, :title, :review
+  validates_presence_of :user
+  validates_presence_of :product
   validates_numericality_of :rating, :only_integer => true
 
   scope :approved,     lambda {|*args| {:conditions => "approved = 't'"}}   
